dc3f5c73048f22e624dd6a5014602751675e53a3
Glitch752 <xxGlitch752xx@gmail.com>
2022-11-16 17:50:26 -0600

    Added search page and recommendations

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,42 @@
 use rocket::fs::FileServer;
+use rocket::serde::{Serialize, json::Json};
 
 #[macro_use] extern crate rocket;
 
 #[cfg(test)] mod tests;
 
-// Try visiting:
-//   http://127.0.0.1:8000/hello/world
+#[derive(Serialize)]
+struct Recommendation<String> {
+    text: String
+}
+
+impl Recommendation<String> {
+    fn new(text: &str) -> Recommendation<String> {
+        Recommendation { text: text.to_string() }
+    }
+}
+
 #[get("/")]
 fn world() -> &'static str {
     "Hello, world!"
 }
 
-// Try visiting:
-//   http://127.0.0.1:8000/wave/Rocketeer/100
-#[get("/<name>/<age>")]
-fn wave(name: &str, age: isize) -> String {
-    format!("👋 Hello, {} year old named {}!", age, name)
+#[get("/recommended?<query>")]
+fn recommended(query: Option<&str>) -> Json<Vec<Recommendation<String>>> {
+    // Return JSON array of recommended search terms for the query
+    // For now, just use the query and "bar"
+    let results = vec![Recommendation::new(query.unwrap_or("foo")), Recommendation::new("bar")];
+    Json(results)
 }
+
+// #[get("/<name>/<age>")]
+// fn wave(name: &str, age: isize) -> String {
+//     format!("👋 Hello, {} year old named {}!", age, name)
+// }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .mount("/", FileServer::from("./public"))
-        .mount("/test", routes![wave, world])
+        .mount("/api", routes![recommended])
 }
