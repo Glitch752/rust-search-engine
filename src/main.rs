db1b2c76048e7d3aeafef827d1e4f5c8820d77f2
Glitch752 <xxGlitch752xx@gmail.com>
2022-11-16 21:49:15 -0600

    Added basic database functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use rocket::fairing::AdHoc;
 use rocket::fs::FileServer;
 use rocket::serde::{Serialize, json::Json};
 
@@ -16,9 +17,18 @@ impl Recommendation<String> {
     }
 }
 
-#[get("/")]
-fn world() -> &'static str {
-    "Hello, world!"
+use rocket_db_pools::sqlx::pool::PoolConnection;
+use rocket_db_pools::{Database};
+use rocket_db_pools::sqlx::{self, Sqlite};
+
+#[derive(Database)]
+#[database("sites")]
+struct Sites(sqlx::SqlitePool);
+
+#[get("/search?<q>")]
+async fn search(/*mut db: Connection<Sites>,*/ q: i64) -> Json<Vec<String>> {
+    // TODO: Implement search
+    Json(vec![q.to_string()])
 }
 
 #[get("/recommended?<query>")]
@@ -29,14 +39,38 @@ fn recommended(query: Option<&str>) -> Json<Vec<Recommendation<String>>> {
     Json(results)
 }
 
-// #[get("/<name>/<age>")]
-// fn wave(name: &str, age: isize) -> String {
-//     format!("👋 Hello, {} year old named {}!", age, name)
-// }
-
-#[launch]
-fn rocket() -> _ {
-    rocket::build()
+#[rocket::main]
+async fn main() {
+    let _ = rocket::build()
         .mount("/", FileServer::from("./public"))
-        .mount("/api", routes![recommended])
+        .mount("/api", routes![recommended, search])
+        .attach(Sites::init())
+        .attach(AdHoc::on_liftoff("Liftoff Printer", |rocket| Box::pin(async move {
+            println!("Liftoff!");
+
+            // Look into the database 'sites' and find the staging table.
+            // If it's empty, then create it.
+            let db: &Sites = Sites::fetch(&rocket).unwrap();
+        
+            let mut connection: PoolConnection<Sqlite> = db.acquire().await.unwrap();
+        
+            // First, get the database connection from rocket.
+            // Then, check if the staging table exists.
+        
+            let staging_table_exists = sqlx::query("SELECT name FROM sqlite_master WHERE type='table' AND name='staging';")
+                .fetch_one(&mut connection)
+                .await
+                .is_ok();
+        
+            if !staging_table_exists {
+                println!("Staging table doesn't exist. Creating it now.");
+                sqlx::query("CREATE TABLE staging (url TEXT PRIMARY KEY);")
+                    .execute(&mut connection)
+                    .await
+                    .unwrap();
+            } else {
+                println!("Staging table exists.");
+            }
+        })))
+        .launch().await;
 }
